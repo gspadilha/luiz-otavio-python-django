0e18227ffe4896d6a18033fb8f1765239f4d1e0c
Guilherme Pro Padilha <gs.padilha@gmail.com>
2023-10-05 10:34:36 -0300

    feat: adicionando renderização de templates

--- _home/views.py
+++ _home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):  # request é sempre obrigatório
-    return HttpResponse('Home!')
+    # return HttpResponse('Home!')
+    return render(request, '_home/index.html')
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    '_home',
+    '_blog',
 ]
 
 MIDDLEWARE = [
